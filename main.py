2bcbb905793a7258eec7392729fdbe7980725a69
KOUSER BIBI <mechezaz@gmail.com>
2026-04-24 14:33:01 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,39 @@
+def on_on_overlap(sprite, otherSprite):
+    mySprite.start_effect(effects.cool_radial, 5000)
+    info.change_score_by(4)
+sprites.on_overlap(SpriteKind.player, SpriteKind.projectile, on_on_overlap)
+
+projectile: Sprite = None
+mySprite: Sprite = None
+scene.set_background_color(10)
+mySprite = sprites.create(img("""
+        8 8 8 . . 8 8 8 8 8 . . . . . .
+        8 9 9 8 8 9 9 9 9 9 8 8 . . . .
+        b 8 9 9 1 9 1 1 1 9 9 9 8 . . .
+        . b 9 9 9 9 1 1 9 9 1 1 9 8 . .
+        . b d 9 9 9 9 9 9 9 9 1 1 9 8 .
+        b 8 9 9 9 9 9 9 9 9 9 9 1 9 8 .
+        c d 9 9 9 9 9 9 9 9 9 9 9 9 9 8
+        c d 8 9 9 9 9 9 9 9 9 9 9 1 9 8
+        c 8 9 9 9 d 9 9 9 9 9 9 9 9 9 8
+        c 8 d 9 8 9 d 9 9 9 9 9 9 9 9 8
+        . c 8 9 9 9 9 d d d 9 9 9 9 9 b
+        . c 8 d 9 8 9 d 8 8 d 9 9 9 8 c
+        . . c 8 8 d 8 8 8 8 8 d d 9 d c
+        . . . c 8 8 8 8 8 8 8 8 9 9 9 8
+        . . . . c c b 8 8 8 b b 8 9 8 8
+        . . . . . . c c c c c c b b 8 .
+        """),
+    SpriteKind.player)
+controller.move_sprite(mySprite)
+mySprite.set_stay_in_screen(True)
+info.start_countdown(180)
+
+def on_update_interval():
+    global projectile
+    projectile = sprites.create_projectile_from_side(assets.image("""
+            Strawberry
+            """),
+        randint(-50, 50),
+        randint(-50, 50))
+game.on_update_interval(500, on_update_interval)
---
+++ main.ts
@@ -0,0 +1,34 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    mySprite.startEffect(effects.coolRadial, 5000)
+    info.changeScoreBy(4)
+})
+let projectile : Sprite = null
+let mySprite : Sprite = null
+scene.setBackgroundColor(10)
+mySprite = sprites.create(img`
+        8 8 8 . . 8 8 8 8 8 . . . . . .
+        8 9 9 8 8 9 9 9 9 9 8 8 . . . .
+        b 8 9 9 1 9 1 1 1 9 9 9 8 . . .
+        . b 9 9 9 9 1 1 9 9 1 1 9 8 . .
+        . b d 9 9 9 9 9 9 9 9 1 1 9 8 .
+        b 8 9 9 9 9 9 9 9 9 9 9 1 9 8 .
+        c d 9 9 9 9 9 9 9 9 9 9 9 9 9 8
+        c d 8 9 9 9 9 9 9 9 9 9 9 1 9 8
+        c 8 9 9 9 d 9 9 9 9 9 9 9 9 9 8
+        c 8 d 9 8 9 d 9 9 9 9 9 9 9 9 8
+        . c 8 9 9 9 9 d d d 9 9 9 9 9 b
+        . c 8 d 9 8 9 d 8 8 d 9 9 9 8 c
+        . . c 8 8 d 8 8 8 8 8 d d 9 d c
+        . . . c 8 8 8 8 8 8 8 8 9 9 9 8
+        . . . . c c b 8 8 8 b b 8 9 8 8
+        . . . . . . c c c c c c b b 8 .
+        `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setStayInScreen(true)
+info.startCountdown(180)
+game.onUpdateInterval(500, function on_update_interval() {
+    
+    projectile = sprites.createProjectileFromSide(assets.image`
+            Strawberry
+            `, randint(-50, 50), randint(-50, 50))
+})
