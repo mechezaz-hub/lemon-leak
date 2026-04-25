0c2c6d29900acf36c4b2b1368af8463fe6a995eb
KOUSER BIBI <mechezaz@gmail.com>
2026-04-25 12:19:25 +0530

    CHANGE!

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,82 @@ e e e e 2 e 2 2 e e e c . . . .
 e e e 2 e e c e c c c . . . . . 
 . c c c c c c c . . . . . . . . 
 `;
+            case "image2":
+            case "Yellow Lemon":return img`
+4 4 4 . . 4 4 4 4 4 . . . . . . 
+4 5 5 4 4 5 5 5 5 5 4 4 . . . . 
+b 4 5 5 1 5 1 1 1 5 5 5 4 . . . 
+. b 5 5 5 5 1 1 5 5 1 1 5 4 . . 
+. b d 5 5 5 5 5 5 5 5 1 1 5 4 . 
+b 4 5 5 5 5 5 5 5 5 5 5 1 5 4 . 
+c d 5 5 5 5 5 5 5 5 5 5 5 5 5 4 
+c d 4 5 5 5 5 5 5 5 5 5 5 1 5 4 
+c 4 5 5 5 d 5 5 5 5 5 5 5 5 5 4 
+c 4 d 5 4 5 d 5 5 5 5 5 5 5 5 4 
+. c 4 5 5 5 5 d d d 5 5 5 5 5 b 
+. c 4 d 5 4 5 d 4 4 d 5 5 5 4 c 
+. . c 4 4 d 4 4 4 4 4 d d 5 d c 
+. . . c 4 4 4 4 4 4 4 4 5 5 5 4 
+. . . . c c b 4 4 4 b b 4 5 4 4 
+. . . . . . c c c c c c b b 4 . 
+`;
+            case "image3":
+            case "Green Lemon":return img`
+8 8 8 . . 8 8 8 8 8 . . . . . . 
+8 7 7 8 8 7 7 7 7 7 8 8 . . . . 
+b 8 7 7 1 7 1 1 1 7 7 7 8 . . . 
+. b 7 7 7 7 1 1 7 7 1 1 7 8 . . 
+. b d 7 7 7 7 7 7 7 7 1 1 7 8 . 
+b 8 7 7 7 7 7 7 7 7 7 7 1 7 8 . 
+c d 7 7 7 7 7 7 7 7 7 7 7 7 7 8 
+c d 8 7 7 7 7 7 7 7 7 7 7 1 7 8 
+c 8 7 7 7 d 7 7 7 7 7 7 7 7 7 8 
+c 8 d 7 8 7 d 7 7 7 7 7 7 7 7 8 
+. c 8 7 7 7 7 d d d 7 7 7 7 7 b 
+. c 8 d 7 8 7 d 8 8 d 7 7 7 8 c 
+. . c 8 8 d 8 8 8 8 8 d d 7 d c 
+. . . c 8 8 8 8 8 8 8 8 7 7 7 8 
+. . . . c c b 8 8 8 b b 8 7 8 8 
+. . . . . . c c c c c c b b 8 . 
+`;
+            case "image4":
+            case "Blue Lemon":return img`
+8 8 8 . . 8 8 8 8 8 . . . . . . 
+8 9 9 8 8 9 9 9 9 9 8 8 . . . . 
+b 8 9 9 1 9 1 1 1 9 9 9 8 . . . 
+. b 9 9 9 9 1 1 9 9 1 1 9 8 . . 
+. b d 9 9 9 9 9 9 9 9 1 1 9 8 . 
+b 8 9 9 9 9 9 9 9 9 9 9 1 9 8 . 
+c d 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+c d 8 9 9 9 9 9 9 9 9 9 9 1 9 8 
+c 8 9 9 9 d 9 9 9 9 9 9 9 9 9 8 
+c 8 d 9 8 9 d 9 9 9 9 9 9 9 9 8 
+. c 8 9 9 9 9 d d d 9 9 9 9 9 b 
+. c 8 d 9 8 9 d 8 8 d 9 9 9 8 c 
+. . c 8 8 d 8 8 8 8 8 d d 9 d c 
+. . . c 8 8 8 8 8 8 8 8 9 9 9 8 
+. . . . c c b 8 8 8 b b 8 9 8 8 
+. . . . . . c c c c c c b b 8 . 
+`;
+            case "p=5qmb_kQHiES,lM?gsT_img":
+            case "Purple Lemon":return img`
+8 8 8 . . 8 8 8 8 8 . . . . . . 
+8 a a 8 8 a a a a a 8 8 . . . . 
+b 8 a a 1 a 1 1 1 a a a 8 . . . 
+. b a a a a 1 1 a a 1 1 a 8 . . 
+. b d a a a a a a a a 1 1 a 8 . 
+b 8 a a a a a a a a a a 1 a 8 . 
+c d a a a a a a a a a a a a a 8 
+c d 8 a a a a a a a a a a 1 a 8 
+c 8 a a a d a a a a a a a a a 8 
+c 8 d a 8 a d a a a a a a a a 8 
+. c 8 a a a a d d d a a a a a b 
+. c 8 d a 8 a d 8 8 d a a a 8 c 
+. . c 8 8 d 8 8 8 8 8 d d a d c 
+. . . c 8 8 8 8 8 8 8 8 a a a 8 
+. . . . c c b 8 8 8 b b 8 a 8 8 
+. . . . . . c c c c c c b b 8 . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,38 +1,52 @@
-info.onCountdownEnd(function on_countdown_end() {
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
+})
+info.onCountdownEnd(function () {
     game.gameOver(true)
     game.setGameOverEffect(true, effects.smiles)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
     mySprite.startEffect(effects.coolRadial, 5000)
     info.changeScoreBy(4)
 })
-let projectile : Sprite = null
-let mySprite : Sprite = null
+controller.player4.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four))
+})
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+})
+controller.player3.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three))
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
 scene.setBackgroundColor(10)
 mySprite = sprites.create(img`
-        8 8 8 . . 8 8 8 8 8 . . . . . .
-        8 9 9 8 8 9 9 9 9 9 8 8 . . . .
-        b 8 9 9 1 9 1 1 1 9 9 9 8 . . .
-        . b 9 9 9 9 1 1 9 9 1 1 9 8 . .
-        . b d 9 9 9 9 9 9 9 9 1 1 9 8 .
-        b 8 9 9 9 9 9 9 9 9 9 9 1 9 8 .
-        c d 9 9 9 9 9 9 9 9 9 9 9 9 9 8
-        c d 8 9 9 9 9 9 9 9 9 9 9 1 9 8
-        c 8 9 9 9 d 9 9 9 9 9 9 9 9 9 8
-        c 8 d 9 8 9 d 9 9 9 9 9 9 9 9 8
-        . c 8 9 9 9 9 d d d 9 9 9 9 9 b
-        . c 8 d 9 8 9 d 8 8 d 9 9 9 8 c
-        . . c 8 8 d 8 8 8 8 8 d d 9 d c
-        . . . c 8 8 8 8 8 8 8 8 9 9 9 8
-        . . . . c c b 8 8 8 b b 8 9 8 8
-        . . . . . . c c c c c c b b 8 .
-        `, SpriteKind.Player)
+    8 8 8 . . 8 8 8 8 8 . . . . . . 
+    8 9 9 8 8 9 9 9 9 9 8 8 . . . . 
+    b 8 9 9 1 9 1 1 1 9 9 9 8 . . . 
+    . b 9 9 9 9 1 1 9 9 1 1 9 8 . . 
+    . b d 9 9 9 9 9 9 9 9 1 1 9 8 . 
+    b 8 9 9 9 9 9 9 9 9 9 9 1 9 8 . 
+    c d 9 9 9 9 9 9 9 9 9 9 9 9 9 8 
+    c d 8 9 9 9 9 9 9 9 9 9 9 1 9 8 
+    c 8 9 9 9 d 9 9 9 9 9 9 9 9 9 8 
+    c 8 d 9 8 9 d 9 9 9 9 9 9 9 9 8 
+    . c 8 9 9 9 9 d d d 9 9 9 9 9 b 
+    . c 8 d 9 8 9 d 8 8 d 9 9 9 8 c 
+    . . c 8 8 d 8 8 8 8 8 d d 9 d c 
+    . . . c 8 8 8 8 8 8 8 8 9 9 9 8 
+    . . . . c c b 8 8 8 b b 8 9 8 8 
+    . . . . . . c c c c c c b b 8 . 
+    `, SpriteKind.Player)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Blue Lemon`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Yellow Lemon`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`Green Lemon`, SpriteKind.Player))
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`Purple Lemon`, SpriteKind.Player))
+mp.setPlayerIndicatorsVisible(true)
 controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
 info.startCountdown(180)
-game.onUpdateInterval(500, function on_update_interval() {
-    
-    projectile = sprites.createProjectileFromSide(assets.image`
-            Strawberry
-            `, randint(-50, 50), randint(-50, 50))
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(assets.image`Strawberry`, randint(-50, 50), randint(-50, 50))
 })
